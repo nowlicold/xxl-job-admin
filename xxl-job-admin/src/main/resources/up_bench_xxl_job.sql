8554dd406781ea2c9beace22a3541ff30101f551
nowlicold <87050471+nowlicold@users.noreply.github.com>
2021-07-22 18:01:49 +0800

    新增分环境机制

--- xxl-job-admin/src/main/resources/up_bench_xxl_job.sql
+++ xxl-job-admin/src/main/resources/up_bench_xxl_job.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `xxl_job`.`xxl_job_group`
+ALTER TABLE `meet_xxl_job_admin`.`xxl_job_group`
 MODIFY COLUMN `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '执行器名称' AFTER `app_name`;
-ALTER TABLE `xxl_job`.`xxl_job_group`
+ALTER TABLE `meet_xxl_job_admin`.`xxl_job_group`
 ADD UNIQUE INDEX `U_APP_NAME`(`app_name`);
 
-ALTER TABLE `xxl_job`.`xxl_job_info`
+ALTER TABLE `meet_xxl_job_admin`.`xxl_job_info`
 ADD UNIQUE INDEX `u_executor_handler`(`executor_handler`);
